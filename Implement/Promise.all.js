bc7c69f34d54be64e515093f190c93bac6b0c9c8
c <2655774991@qq.com>
2025-09-04 23:56:05 +0800

    2025-09-04

---
+++ Implement/Promise.all.js
@@ -0,0 +1,43 @@
+Promise.myAll = function (promiseArr) {
+  return new Promise((resolve, reject) => {
+    if (promiseArr == null || typeof promiseArr[Symbol.iterator] !== 'function')
+      throw new TypeError(`${promiseArr} is not an iterator`);
+    const ans = [];
+    let index = 0;
+    for (let i = 0; i < promiseArr.length; i++) {
+      promiseArr[i]
+        .then((res) => {
+          ans[i] = res;
+          index++;
+          if (index === promiseArr.length) {
+            resolve(ans);
+          }
+        })
+        .catch((err) => reject(err));
+    }
+  });
+};
+
+const p1 = Promise.resolve(1);
+const p2 = new Promise((resolve) => {
+  setTimeout(() => resolve(2), 1000);
+});
+const p3 = new Promise((resolve) => {
+  setTimeout(() => resolve(3), 3000);
+});
+
+const p4 = Promise.reject('err4');
+const p5 = Promise.reject('err5');
+// 1. 所有的Promise都成功了
+const p11 = Promise.myAll([p1, p2, p3])
+  .then(console.log) // [ 1, 2, 3 ]
+  .catch(console.log);
+
+// 2. 有一个Promise失败了
+const p12 = Promise.myAll([p1, p2, p4]).then(console.log).catch(console.log); // err4
+
+// 3. 有两个Promise失败了，可以看到最终输出的是err4，第一个失败的返回值
+const p13 = Promise.myAll([p1, p4, p5]).then(console.log).catch(console.log); // err4
+// 与原生的Promise.all返回是一致的
+
+const p14 = Promise.myAll().then(console.log);
